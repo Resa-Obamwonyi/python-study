908e54b6c2dfd964fd00f0be8768c8f2b187fd08
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-08-02 11:57:39 +0100

    first python commit

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#Basic Calculator
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = num1 + num2
+
+print(result)
---
+++ main.py
@@ -0,0 +1,15 @@
+x = "Theresa"
+print("Hello " + x)
+
+#Defining Functions
+def my_function(str_length):
+    print(len(str_length))
+    print(str_length.index("T"))
+
+
+my_function("Hello Theresa")
+
+#Getting Inputs from Users
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+print("Hello " + name + ", You are " + age + " years old.")
