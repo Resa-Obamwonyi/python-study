2ddd40ff3892f56fd97dd135306dd3196d2ebf19
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-08-04 12:10:37 +0100

    lists to set algorithm

---
+++ set.py
@@ -0,0 +1,9 @@
+my_list = [2, 5, 7, 9, 0, 1, 2, 6, 7, 8, 9, 2, 4, 3, 3, 5, 66, 76, 66]
+
+
+def convert_lists_to_set(ownlist):
+    return set(ownlist)
+
+
+print(convert_lists_to_set(my_list))
+print(convert_lists_to_set([16, 16, 16, 16, 18, 18, 19, 9, 8, 9, 0]))
