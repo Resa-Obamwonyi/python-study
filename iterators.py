156fa0b66466cef7445dd0eb4ad9219b6c98926f
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2020-08-21 18:10:12 +0100

    iterators

---
+++ iterators.py
@@ -0,0 +1,38 @@
+# box = []
+# def iterate():
+#     for i in 'python':
+#         box.append(i)
+#     return box
+
+
+# print(iterate())
+
+
+
+# class iterateSomething:
+#     def __init__(self,box):
+#         self.box = box
+#         self.firstnum = -1
+    
+#     def __iter__(self):
+#         return self
+
+#     def __next__(self):
+#         self.firstnum += 1
+#         return self.box[self.firstnum]
+
+
+# a=[1,2,3,4,5,6]
+# myList = iterateSomething(a)
+# it = iter(myList)
+
+
+
+my_iter = iter([1,2,3,4,5,6])
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
